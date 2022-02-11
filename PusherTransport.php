3cc36e16f6ba93e61f5815f4a47ded3cdc396192
Yasmany Cubela Medina <yasmanycm@gmail.com>
2022-02-10 23:05:26 -0500

    implement pusher transport

--- PusherTransport.php
+++ PusherTransport.php
@@ -5,16 +5,16 @@ declare(strict_types=1);
 namespace Symfony\Component\Notifier\Bridge\Pusher;
 
 use Pusher\Pusher;
+use RuntimeException;
 use Symfony\Component\Notifier\Exception\LogicException;
-use Symfony\Component\Notifier\Exception\TransportException;
 use Symfony\Component\Notifier\Exception\UnsupportedMessageTypeException;
 use Symfony\Component\Notifier\Message\MessageInterface;
 use Symfony\Component\Notifier\Message\PushMessage;
 use Symfony\Component\Notifier\Message\SentMessage;
 use Symfony\Component\Notifier\Transport\AbstractTransport;
 use Symfony\Contracts\EventDispatcher\EventDispatcherInterface;
-use Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface;
 use Symfony\Contracts\HttpClient\HttpClientInterface;
+use Throwable;
 
 /**
  * @author Yasmany Cubela Medina <yasmanycm@gmail.com>
@@ -45,49 +45,27 @@ final class PusherTransport extends AbstractTransport
 
     protected function doSend(MessageInterface $message): SentMessage
     {
-//        if (!$message instanceof PushMessage) {
-//            throw new UnsupportedMessageTypeException(__CLASS__, PushMessage::class, $message);
-//        }
-//
-//        if ($message->getOptions() && !$message->getOptions() instanceof PusherOptions) {
-//            throw new LogicException(sprintf('The "%s" transport only supports instances of "%s" for options.', __CLASS__, PusherOptions::class));
-//        }
-//
-//        if (!($opts = $message->getOptions()) && $notification = $message->getNotification()) {
-//            $opts = PusherOptions::fromNotification($notification);
-//        }
-//
-//        $options = $opts ? $opts->toArray() : [];
-//        if (!isset($options['channel'])) {
-//            $options['channel'] = $message->getRecipientId() ?: $this->chatChannel;
-//        }
-//        $options['text'] = $message->getSubject();
-//        $response = $this->client->request('POST', 'https://'.$this->getEndpoint().'/api/chat.postMessage', [
-//            'json' => array_filter($options),
-//            'auth_bearer' => $this->accessToken,
-//            'headers' => [
-//                'Content-Type' => 'application/json; charset=utf-8',
-//            ],
-//        ]);
-//
-//        try {
-//            $statusCode = $response->getStatusCode();
-//        } catch (TransportExceptionInterface $e) {
-//            throw new TransportException('Could not reach the remote Pusher server.', $response, 0, $e);
-//        }
-//
-//        if (200 !== $statusCode) {
-//            throw new TransportException(sprintf('Unable to post the Pusher message: "%s".', $response->getContent(false)), $response);
-//        }
-//
-//        $result = $response->toArray(false);
-//        if (!$result['ok']) {
-//            $errors = isset($result['errors']) ? ' ('.implode('|', $result['errors']).')' : '';
-//
-//            throw new TransportException(sprintf('Unable to post the Pusher message: "%s"%s.', $result['error'], $errors), $response);
-//        }
-//
-        return new SentMessage($message, (string) $this);
-//        $sentMessage->setMessageId($result['ts']);
+        if (!$message instanceof PushMessage) {
+            throw new UnsupportedMessageTypeException(__CLASS__, PushMessage::class, $message);
+        }
+
+        if ($message->getOptions() && !$message->getOptions() instanceof PusherOptions) {
+            throw new LogicException(sprintf('The "%s" transport only supports instances of "%s" for options.', __CLASS__, PusherOptions::class));
+        }
+
+        if (!($options = $message->getOptions()) && $notification = $message->getNotification()) {
+            $options = PusherOptions::fromNotification($notification);
+        }
+
+        try {
+            $response = $this->pusherClient->trigger($options['channel'], $message->getSubject(), json_encode($message->getContent(), JSON_THROW_ON_ERROR));
+        } catch (Throwable) {
+            throw new RuntimeException('An error occurred at Pusher Notifier Transport');
+        }
+
+        $sentMessage = new SentMessage($message, $this->__toString());
+        $sentMessage->setMessageId($response['ts']);
+
+        return $sentMessage;
     }
 }
