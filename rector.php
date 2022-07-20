efa831e57bfcbfb1c759f0516a2686535b00d70c
Yasmany Cubela Medina <yasmanycm@gmail.com>
2022-07-19 22:46:25 -0500

    update packages csf

--- rector.php
+++ rector.php
@@ -2,48 +2,41 @@
 
 declare(strict_types=1);
 
-use Rector\Core\Configuration\Option;
+use Rector\Config\RectorConfig;
 use Rector\Set\ValueObject\SetList;
-use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 
-return static function (ContainerConfigurator $containerConfigurator): void {
-    $containerConfigurator->import(SetList::PHP_71);
-    $containerConfigurator->import(SetList::PHP_72);
-    $containerConfigurator->import(SetList::PHP_73);
-    $containerConfigurator->import(SetList::PHP_74);
-    $containerConfigurator->import(SetList::PHP_80);
-    $containerConfigurator->import(SetList::PHP_81);
-    $containerConfigurator->import(SetList::PSR_4);
-    $containerConfigurator->import(SetList::CODE_QUALITY);
-    $containerConfigurator->import(SetList::CODING_STYLE);
-    $containerConfigurator->import(SetList::PRIVATIZATION);
-    $containerConfigurator->import(SetList::UNWRAP_COMPAT);
-    $containerConfigurator->import(SetList::DEAD_CODE);
-    $containerConfigurator->import(SetList::EARLY_RETURN);
-    $containerConfigurator->import(SetList::NAMING);
-    $containerConfigurator->import(SetList::TYPE_DECLARATION);
-    $containerConfigurator->import(SetList::MONOLOG_20);
-
-    $parameters = $containerConfigurator->parameters();
-    $parameters->set(Option::PARALLEL, true);
-    $parameters->set(
-        Option::SKIP,
+return static function (RectorConfig $rectorConfig): void {
+    $rectorConfig->sets(
         [
+            SetList::PHP_71,
+            SetList::PHP_72,
+            SetList::PHP_73,
+            SetList::PHP_74,
+            SetList::PHP_80,
+            SetList::PHP_81,
+            SetList::PSR_4,
+            SetList::CODE_QUALITY,
+            SetList::CODING_STYLE,
+            SetList::PRIVATIZATION,
+            SetList::UNWRAP_COMPAT,
+            SetList::DEAD_CODE,
+            SetList::EARLY_RETURN,
+            SetList::NAMING,
+            SetList::TYPE_DECLARATION,
+            SetList::MONOLOG_20,
+        ]
+    );
+
+    $rectorConfig->parallel();
+    $rectorConfig->skip([
             __DIR__.'/vendor/*',
             __DIR__.'/phpinsights.php',
         ]
     );
 
-    $services = $containerConfigurator->services();
-
-    $services->set(Rector\Php74\Rector\LNumber\AddLiteralSeparatorToNumberRector::class)
-        ->call(
-            'configure',
-            [
-                [
-                    Rector\Php74\Rector\LNumber\AddLiteralSeparatorToNumberRector::LIMIT_VALUE => 100,
-                ],
-            ]
-        )
-    ;
+    $rectorConfig->ruleWithConfiguration(Rector\Php74\Rector\LNumber\AddLiteralSeparatorToNumberRector::class, [
+        [
+            Rector\Php74\Rector\LNumber\AddLiteralSeparatorToNumberRector::LIMIT_VALUE => 100,
+        ],
+    ]);
 };
